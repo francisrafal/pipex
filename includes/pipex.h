68ea06efb7453a4aa2433e1b4a483c8c3349014a
Francis Rafal <francis.rafal@gmail.com>
2022-11-29 16:25:50 +0100

    feat: Test Two Child Processes

--- includes/pipex.h
+++ includes/pipex.h
@@ -17,6 +17,7 @@
 # include <string.h>
 # include <sys/types.h>
 # include <sys/stat.h>
+# include <sys/wait.h>
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdlib.h>
--- src/main.c
+++ src/main.c
@@ -14,8 +14,9 @@
 
 int	main(int argc, char **argv)
 {
-	int	fd;
+	//int	fd;
 	int	pipefd[2];
+	(void)argv;
 
 	if (argc != 5)
 	{
@@ -27,12 +28,75 @@ int	main(int argc, char **argv)
 		perror("pipe");
 		exit(EXIT_FAILURE);
 	}
+	/*
 	ft_printf("0: %d, 1: %d\n", pipefd[0], pipefd[1]);
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
 		perror("open");
 		exit(EXIT_FAILURE);
+	}
+	*/
+	int arr[] = {1, 2, 3, 4, 5, 6};
+	int id1 = fork();
+	if (id1 == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	int id2;
+	id2 = -1;
+	if (id1 != 0)
+	{
+		id2 = fork();	
+		if (id2 == -1)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+	}
+	int	start;
+	int	end;
+	if (id1 == 0)
+	{
+		start = 0;
+		end = sizeof(arr) / sizeof(int) / 2;
+	}
+	if (id2 == 0)
+	{
+		start = sizeof(arr) / sizeof(int) / 2;
+		end = sizeof(arr) / sizeof(int);
+	}
+	int	sum;
+	sum = 0;
+	if (id1 == 0 || id2 == 0)
+	{
+		int	i;
+		i = start;
+		while (i < end)
+		{
+			sum += arr[i];
+			i++;
+		}
+		ft_printf("sum: %d\n", sum);
+		close(pipefd[0]);
+		write(pipefd[1], &sum, sizeof(sum));	
+		close(pipefd[1]);
+	}
+	else
+	{
+		int sum_child;
+		close(pipefd[1]);
+		if (read(pipefd[0], &sum_child, sizeof(sum_child) == -1))
+		{
+			perror("read");
+			exit(EXIT_FAILURE);
+		}
+		close(pipefd[0]);
+		sum += sum_child;
+		ft_printf("sum: %d\n", sum);
+		wait(NULL);
+		//waitpid(id1, NULL, 0);
 	}
 	return (0);
 }
