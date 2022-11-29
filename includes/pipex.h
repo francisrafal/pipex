d6ac13a8966ce476fb34dce31216112cddccb5de
Francis Rafal <francis.rafal@gmail.com>
2022-11-29 15:53:16 +0100

    feat: Add Pipe

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,8 +6,20 @@
 /*   By: frafal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 16:50:42 by frafal            #+#    #+#             */
-/*   Updated: 2022/11/28 16:50:45 by frafal           ###   ########.fr       */
+/*   Updated: 2022/11/29 10:36:41 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PIPEX_H
+# define PIPEX_H
+# include <errno.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include "libft.h"
 
+#endif
--- src/main.c
+++ src/main.c
@@ -12,7 +12,27 @@
 
 #include "pipex.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
+	int	fd;
+	int	pipefd[2];
+
+	if (argc != 5)
+	{
+		ft_putstr_fd("usage: ./pipex <file1> \"cmd1\" \"cmd2\" <file2>\n", 2);
+		exit (EXIT_FAILURE);
+	}
+	if (pipe(pipefd) == -1)
+	{
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
+	ft_printf("0: %d, 1: %d\n", pipefd[0], pipefd[1]);
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		perror("open");
+		exit(EXIT_FAILURE);
+	}
 	return (0);
 }
