9d0f06e6190471b56860460747b27b512b5e4a16
Francis Rafal <francis.rafal@gmail.com>
2022-11-30 17:49:20 +0100

    feat: IPC

--- src/main.c
+++ src/main.c
@@ -6,15 +6,38 @@
 /*   By: frafal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 16:50:19 by frafal            #+#    #+#             */
-/*   Updated: 2022/11/28 16:52:21 by frafal           ###   ########.fr       */
+/*   Updated: 2022/11/30 17:47:41 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+void	child_process(int pipefd[])
+{
+	char	buf;
+
+	close(pipefd[1]);
+	ft_putstr_fd("I received this from my parent:\n", STDOUT_FILENO);
+	while (read(pipefd[0], &buf, 1) > 0)
+		ft_putchar_fd(buf, STDOUT_FILENO);
+	close(pipefd[0]);
+	exit(EXIT_SUCCESS);
+}
+
+void	parent_process(int pipefd[])
+{
+	close(pipefd[0]);
+	ft_putstr_fd("Let's write a secret\n", STDOUT_FILENO);
+	ft_putstr_fd("I am your father!\n", pipefd[1]);
+	close(pipefd[1]);
+	//wait(NULL);
+	exit(EXIT_SUCCESS);
+}
+
 int	main(int argc, char **argv)
 {
 	//int	fd;
+	pid_t	pid;
 	int	pipefd[2];
 	(void)argv;
 
@@ -28,6 +51,16 @@ int	main(int argc, char **argv)
 		perror("pipe");
 		exit(EXIT_FAILURE);
 	}
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	if (pid == 0)
+		child_process(pipefd);
+	else
+		parent_process(pipefd);
 	/*
 	ft_printf("0: %d, 1: %d\n", pipefd[0], pipefd[1]);
 	fd = open(argv[1], O_RDONLY);
@@ -37,66 +70,5 @@ int	main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 	*/
-	int arr[] = {1, 2, 3, 4, 5, 6};
-	int id1 = fork();
-	if (id1 == -1)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-	int id2;
-	id2 = -1;
-	if (id1 != 0)
-	{
-		id2 = fork();	
-		if (id2 == -1)
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-	}
-	int	start;
-	int	end;
-	if (id1 == 0)
-	{
-		start = 0;
-		end = sizeof(arr) / sizeof(int) / 2;
-	}
-	if (id2 == 0)
-	{
-		start = sizeof(arr) / sizeof(int) / 2;
-		end = sizeof(arr) / sizeof(int);
-	}
-	int	sum;
-	sum = 0;
-	if (id1 == 0 || id2 == 0)
-	{
-		int	i;
-		i = start;
-		while (i < end)
-		{
-			sum += arr[i];
-			i++;
-		}
-		ft_printf("sum: %d\n", sum);
-		close(pipefd[0]);
-		write(pipefd[1], &sum, sizeof(sum));	
-		close(pipefd[1]);
-	}
-	else
-	{
-		int sum_child;
-		close(pipefd[1]);
-		if (read(pipefd[0], &sum_child, sizeof(sum_child) == -1))
-		{
-			perror("read");
-			exit(EXIT_FAILURE);
-		}
-		close(pipefd[0]);
-		sum += sum_child;
-		ft_printf("sum: %d\n", sum);
-		wait(NULL);
-		//waitpid(id1, NULL, 0);
-	}
 	return (0);
 }
