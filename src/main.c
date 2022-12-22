bf152a5a40813b26a4253f6ddf828153f96cfeae
Francis Rafal <francis.rafal@gmail.com>
2022-12-22 16:50:42 +0100

    feat: Add execve (not working yet)

--- src/main.c
+++ src/main.c
@@ -18,41 +18,42 @@ void	perror_exit(char *err)
 	exit(EXIT_FAILURE);
 }
 
-void	child1_process(t_data data)
+void	child1_process(t_data data, char **argv, char **envp)
 {
-	dup2(data.pipefd[1], 1);
 	close(data.pipefd[0]);
-	dup2(data.file1, 0);
-	ft_putstr_fd("Hello1\n", STDOUT_FILENO);
-	close(data.pipefd[0]);
-	exit(EXIT_SUCCESS);
+	dup2(data.file1, STDIN_FILENO);
+	dup2(data.pipefd[1], STDOUT_FILENO);
+	data.cmd_args = ft_split(argv[2], ' ');
+	ft_printf((data.cmd_args)[0]);
+	execve("/usr/bin/ls", data.cmd_args, envp);
+	//ft_putstr_fd("Hello1\n", STDOUT_FILENO);
 }
 
-void	child2_process(t_data data)
+void	child2_process(t_data data, char **argv, char **envp)
 {
+	//char	buf[8];
+
 	close(data.pipefd[1]);
-	ft_putstr_fd("Hello2\n", STDOUT_FILENO);
-	close(data.pipefd[0]);
-	exit(EXIT_SUCCESS);
+	dup2(data.file2, STDOUT_FILENO);
+	dup2(data.pipefd[0], STDIN_FILENO);
+	data.cmd_args = ft_split(argv[3], ' ');
+	ft_printf((data.cmd_args)[0]);
+	execve("/usr/bin/wc", data.cmd_args, envp);
+	//read(STDIN_FILENO, buf, 8);
+	//ft_putstr_fd(buf, STDOUT_FILENO);
 }
 
 void	parent_process(t_data data)
 {
-	data.pid2 = fork();
-	if (data.pid2 == -1)
-		perror_exit("fork");
-	if (data.pid2 == 0)
-		child2_process(data);
-	waitpid(data.pid1, &(data.wstatus), 0);	
-	waitpid(data.pid2, &(data.wstatus), 0);	
+	waitpid(data.pid1, &(data.wstatus), 0);
+	waitpid(data.pid2, &(data.wstatus), 0);
 
 	close(data.pipefd[0]);
-	ft_putstr_fd("Let's write a secret\n", STDOUT_FILENO);
 	close(data.pipefd[1]);
 	exit(EXIT_SUCCESS);
 }
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **envp)
 {
 	t_data	data;
 
@@ -61,11 +62,11 @@ int	main(int argc, char **argv)
 		ft_putstr_fd("usage: ./pipex <file1> \"cmd1\" \"cmd2\" <file2>\n", 2);
 		exit (EXIT_FAILURE);
 	}
-	data.file1 = open(argv[1], O_RDONLY);	
+	data.file1 = open(argv[1], O_RDONLY);
 	if (data.file1 == -1)
 		perror_exit("open file1");
 	data.file2 = open(argv[4], O_WRONLY | O_TRUNC | O_CREAT,
-		S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);	
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 	if (data.file2 == -1)
 		perror_exit("open file2");
 	if (pipe(data.pipefd) == -1)
@@ -74,8 +75,15 @@ int	main(int argc, char **argv)
 	if (data.pid1 == -1)
 		perror_exit("fork");
 	if (data.pid1 == 0)
-		child1_process(data);
+		child1_process(data, argv, envp);
 	else
+	{
+		data.pid2 = fork();
+		if (data.pid2 == -1)
+			perror_exit("fork");
+		if (data.pid2 == 0)
+			child2_process(data, argv, envp);
 		parent_process(data);
+	}
 	return (0);
 }
